0c22f65db516d71bc4185850b4c6ef65de12fd9b
antonio <antonio_ajb@outlook.com>
2024-10-15 11:25:45 +0200

    Registro y login

--- app/src/main/java/com/antoniojb/first/Login.java
+++ app/src/main/java/com/antoniojb/first/Login.java
@@ -1,6 +1,10 @@
 package com.antoniojb.first;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +20,35 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        Button loginButton = findViewById(R.id.loginButton);
+        TextView loginRegisterText = findViewById(R.id.loginRegisterText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMain();
+            }
+        });
+
+        loginRegisterText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchRegister();
+            }
+        });
+    }
+
+
+
+    public void launchMain() {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    public void launchRegister() {
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
     }
 }
